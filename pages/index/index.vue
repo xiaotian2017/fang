d28481b823d248cad17751b9e1ded16c06a09a48
caoyanyuan <0049003436>
2020-06-14 13:32:50 +0800

    icones

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			 <input class="uni-input" focus placeholder="自动" />
 		</view>
 	</view>
 </template>
---
+++ pages/question/question.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		提问界面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
