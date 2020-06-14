a926d5bc61945282cd625b9b367c2c8799a89e92
caoyanyuan <0049003436>
2020-06-14 12:50:58 +0800

    tabbar配置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,7 +11,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello world'
 			}
 		},
 		onLoad() {
@@ -23,7 +23,7 @@
 	}
 </script>
 
-<style>
+<style lang="less">
 	.content {
 		display: flex;
 		flex-direction: column;
